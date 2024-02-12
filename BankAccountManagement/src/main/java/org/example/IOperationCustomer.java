7a1fdae64e053db5ae493a4ab28c8315fadccd8a
Carlariela <carlarielita@gmail.com>
2024-02-11 22:00:52 -0400

    interfaces

---
+++ BankAccountManagement/src/main/java/org/example/IOperationCustomer.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public interface IOperationCustomer {
+    public String getFullName();
+    public void makeSavingDeposits();
+    public void makeSavingsWithdrawals();
+    public void makeCurrentWithdrawals();
+    public void makeCreditCardPayments();
+    public void requestLoan();
+    public void makeLoanPayments();
+}
