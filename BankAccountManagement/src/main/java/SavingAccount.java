bab519a387ab957eabb4c2b871431f2232f55216
carlosHernandez17 <carlose.hernandez09@gmail.com>
2024-02-12 21:37:49 -0600

    class Account and SavingAccount

---
+++ BankAccountManagement/src/main/java/SavingAccount.java
@@ -0,0 +1,9 @@
+public class SavingAccount  extends  Account{
+
+    public SavingAccount() {
+    }
+
+    public SavingAccount(int accountType, String accountNumber, String debitCard, String creditCard, Double balance, String holder) {
+        super(accountType, accountNumber, debitCard, creditCard, balance, holder);
+    }
+}
