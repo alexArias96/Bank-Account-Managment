129c4a550692b0ed02e88a0956399aaa6f4a18da
Carlariela <carlarielita@gmail.com>
2024-02-16 03:38:35 -0400

    IOpAdm

--- BankAccountManagement/src/main/java/org/example/IOperationAdministrator.java
+++
@@ -1,9 +0,0 @@
-public interface IOperationAdministrator {
-    public void registerCustomers();
-    public void removeCustomers();
-    public void createSavingsAccount();
-    public void createCurrentAccount();
-    public void createDebitCard();
-    public void createCreditCard();
-    public void createLoan();
-}
